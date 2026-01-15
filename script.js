e7f0e9205d9a18c3917c82412580c9ea9bb15d64
TheRedQueen <pand3m1x@gmail.com>
2026-01-14 20:43:42 -0500

    C:/Program Files/Git/|/ ^._.^ /|/ Added a bunch of stuff to CSS, Java, and HTML. Still having problems with getting certain buttons to populate correctly. /|/ ^._.^ /|/

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // script
+
+function addTask(){
+  let taskInput = document.getElementById("task").value;
+  let dueDate = document.getElementById("due-date").value;
+  let category = document.getElementById("category").value;
+  let status = document.getElementById("status").value;
+  let taskList = document.getElementById("task-list");
+
+
+
+  if (taskInput && dueDate) {
+    let li = document.createElement("li");
+    li.innerHTML = (`${taskInput} - ${category} - ${status}<span>${new Date(dueDate).toLocaleDateString()}</span>`);
+    taskList.appendChild(li);
+
+    let timeDiff= new Date(dueDate) - new Date();
+    if (timeDiff > 0) {
+      setTimeout(() => alert(`Master Wayne, your task "${taskInput}" is due today!`), timeDiff);
+    }
+  }
+}
+
+let button1 = document.getElementById("addbtn");
+
+button1.addEventListener("click",addTask);
