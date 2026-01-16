c7d8ad148a3295832b51f3165d97ffbf578c2f49
TheRedQueen <pand3m1x@gmail.com>
2026-01-16 18:46:16 -0500

    /|\ ^._.^ /|\ still can't figure out edit button. For another day. Did get cursor hover edits made. And restructured the JS to contain correct order of buttons... /|\ ^._.^ /|\

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 let taskList = document.getElementById("task-list");
 
+//general task function
+
 function addTask(){
   let taskInput = document.getElementById("task").value;
   let dueDate = document.getElementById("due-date").value;
@@ -37,21 +39,44 @@ function addTask(){
 
   dropdown.value = status
 
+      // <select id="category">
+      //       <option value="work">Work</option>
+      //       <option value="Personal">Personal</option>
+      //       <option value="Urgent">Urgent</option>
+      //   </select>
+      //do I need this to make the edit button work for categorys? 
 
   if (taskInput && dueDate) {
     let li = document.createElement("li");
-    li.innerHTML = (`${taskInput} - ${category} <span>${new Date(dueDate).toLocaleDateString()}</span>`); //if I want back in task bar - ${status}
-    li.appendChild(dropdown)
-    taskList.appendChild(li);
+    li.innerHTML = (`${taskInput} - ${category} <span>${new Date(dueDate).toLocaleDateString()}</span>`); // (if I want back in task bar) - ${status}
+    li.appendChild(dropdown);
+
+    let editbtn = document.createElement("editbtn");
+    editbtn.id = "editbtn"
+    editbtn.innerText = "✏️";
+    editbtn.onclick = function(){
+      // let span = prompt("Edit your task:", `${taskInput} - ${category}`); //Is this where I try to splice?
+    }
     li.appendChild(editbtn);
+   
+    let trashbtn = document.createElement("deletebtn");
+    trashbtn.id = "trashbtn"; //this should let me edit in css?
+    trashbtn.innerText = "🗑️";
+    trashbtn.onclick = function(event){
+      event.target.parentElement.remove();
+    }
     li.appendChild(trashbtn);
+
+    taskList.appendChild(li);
     console.log(li);
 
     let timeDiff= new Date(dueDate) - new Date();
     if (timeDiff > 0) {
       setTimeout(() => alert(`Master Wayne, your task "${taskInput}" is due today!`), timeDiff);
+      console.log(timeDiff);
     }
     
+
     // let editbtn = document.createElement("editbutton");
     //   editbtn.value = "edit";
     //   editbtn.innerText = "✏️";
@@ -71,27 +96,29 @@ function addTask(){
   }
 }
 
+//add button for tasks
 let button1 = document.getElementById("addbtn");
 
 button1.addEventListener("click",addTask);
 
+// Move below into function add task and stop fighting the function for use of its variables?
+// // tricky edit button
+// let editbtn = document.createElement("editbutton"); // do I need to add a span element? Do I need to replicate what exists in the function?
+//   editbtn.value = "edit";
+//   editbtn.innerText = "✏️";
+//   editbtn.onclick = function(event){
+//     console.log(event.target)
+//     event.target.parentElement.append(li)
+//   }
 
-let editbtn = document.createElement("editbutton");
-  editbtn.value = "edit";
-  editbtn.innerText = "✏️";
-  editbtn.onclick = function(event){
-    console.log(event.target)
-    event.target.parentElement.append(li)
-  }
-
-
-let trashbtn = document.createElement("deletebutton");
-  trashbtn.value = "delete";
-  trashbtn.innerText = "🗑️";
-  trashbtn.onclick = function(event){
-    console.log(event.target)
-    event.target.parentElement.remove()
-  }
+// // delete button (And it works!)
+// let trashbtn = document.createElement("deletebutton");
+//   trashbtn.value = "delete";
+//   trashbtn.innerText = "🗑️";
+//   trashbtn.onclick = function(event){
+//     console.log(event.target)
+//     event.target.parentElement.remove()
+//   }
 
 
 // li.appendChild(editbtn);
@@ -104,6 +131,7 @@ let trashbtn = document.createElement("deletebutton");
 // let editbtn = document.getElementById("editbtn");
 // editbtn.addEventListener("click", editTask);
 
+// Clear all tasks button (and this works too!)
 function clearTasks(){
   let taskList = document.getElementById("task-list");
   taskList.innerHTML = "";
